d6f2096232e3c80bbde4e562175a85205e6ed789
Rafael Magalhães <rnunesmagalhaes@gmail.com>
2023-10-18 19:21:51 -0300

    Ocultar resposta da tarefa

--- Aula 2/script.R
+++ Aula 2/script.R
@@ -2,6 +2,4 @@ install.packages("tidyverse")
 
 library(tidyverse)
 
-banco_mundial <- read_csv2("Aula 2/wb-brazil.csv")
-
-mean(banco_mundial$lendprojectcost)
+banco_mundial <- read_csv2("wb-brazil.csv") # read_csv2("Aula 2/wb-brazil.csv")
